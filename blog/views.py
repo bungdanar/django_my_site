574d9a303621e5b6ca6a207865c3fc46b6483250
Bung Danar <danar.g.putera@gmail.com>
2021-05-13 16:08:03 +0700

    End of Section 5

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from datetime import date
 from django.shortcuts import render
 from django.http import HttpRequest
 
-posts_data = [
+all_posts = [
     {
         "slug": "hike-in-the-mountains",
         "image": "mountains.jpg",
@@ -71,7 +71,7 @@ posts_data = [
 
 
 def starting_page(request: HttpRequest):
-    sorted_posts = sorted(posts_data, key=lambda post: post['date'])
+    sorted_posts = sorted(all_posts, key=lambda post: post['date'])
     latest_posts = sorted_posts[-3:]
     return render(request, "blog/index.html", {
         "posts": latest_posts
@@ -79,8 +79,13 @@ def starting_page(request: HttpRequest):
 
 
 def posts(request: HttpRequest):
-    return render(request, "blog/all-posts.html")
+    return render(request, "blog/all-posts.html", {
+        "all_posts": all_posts
+    })
 
 
 def post_detail(request: HttpRequest, slug: str):
-    return render(request, "blog/post-detail.html")
+    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    return render(request, "blog/post-detail.html", {
+        "post": identified_post
+    })
