f814275ec0913b716c85e4162e796415261a39ee
Bung Danar <danar.g.putera@gmail.com>
2021-05-13 10:49:34 +0700

    Section 5: Adding Images as Static Files

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpRequest
 
-# Create your views here.
+
+def starting_page(request: HttpRequest):
+    return render(request, "blog/index.html")
+
+
+def posts(request: HttpRequest):
+    pass
+
+
+def post_detail(request: HttpRequest):
+    pass
